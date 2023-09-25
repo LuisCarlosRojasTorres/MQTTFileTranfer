5aa4ea07af094d3d0b7f0ce7dde964af2503a0fe
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-09-25 19:20:07 -0300

    Added Client

---
+++ Client/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Client.Model;
+using Client.TypeOfClients;
+using Newtonsoft.Json;
+
+BrokerOptions brokerOptions;
+
+using (StreamReader file = File.OpenText(Path.Combine("BrokerConfig.json")))
+{
+    brokerOptions = JsonConvert.DeserializeObject<BrokerOptions>(file.ReadToEnd());
+    Console.WriteLine($" - Broker IP Adress: {brokerOptions.Ip}");
+    Console.WriteLine($" - Broker Port: {brokerOptions.Port}");
+}
+
+//await ClientNoKeys.Ping_Server(brokerOptions);
+
+await ClientNoKeys.Connect_Client(brokerOptions);
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
---
+++ Client/Client/TypeOfClients/ClientNoKeys.cs
@@ -0,0 +1,76 @@
+﻿using Client.Model;
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace Client.TypeOfClients
+{
+    public static class ClientNoKeys
+    {
+        public static async Task Ping_Server( BrokerOptions brokerOptions )
+        {
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(brokerOptions.Ip, brokerOptions.Port).Build();
+
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                await mqttClient.PingAsync(CancellationToken.None);
+
+                if (mqttClient.IsConnected) { Console.WriteLine(">> Client connected!!"); }
+
+                var mqttClientDisconnectOptions = mqttFactory.CreateClientDisconnectOptionsBuilder().Build();
+                await mqttClient.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
+            }
+        }
+
+        public static async Task Connect_Client( BrokerOptions brokerOptions )
+        {
+            
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(brokerOptions.Ip, brokerOptions.Port).Build();
+
+                // Setup message handling before connecting so that queued messages
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine(">> Received application message.");
+                    e.DumpToConsole();
+
+                    return Task.CompletedTask;
+                };                
+               
+                var connectResponse = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+                //connectResponse.DumpToConsole();                
+
+                if (mqttClient.IsConnected) { Console.WriteLine(">> Client connected!!"); }
+
+                // Create the subscribe options including several topics with different options.
+                // It is also possible to all of these topics using a dedicated call of _SubscribeAsync_ per topic.
+                var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                    .WithTopicFilter(
+                        f =>
+                        {
+                            f.WithTopic("HelloWorld");                            
+                        })
+                    .Build();
+
+                var suscribedResponse = await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+                
+                // The response contains additional data sent by the server after subscribing.
+                
+                Console.WriteLine(">> Press enter to exit.");
+                Console.ReadLine();
+
+                var mqttClientDisconnectOptions = mqttFactory.CreateClientDisconnectOptionsBuilder().Build();
+                await mqttClient.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
+                Console.WriteLine(">> Client Disconected.");
+            }
+        }
+
+
+    }
+}
